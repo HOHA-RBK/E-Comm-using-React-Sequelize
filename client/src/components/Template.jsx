ade332da7838ff0aa0adbb91c29bec741b4a73d1
assilbenshilibenmohamedlaabidielabed <assilbenshilielabed@gmail.com>
2024-08-01 12:00:59 +0200

    one product template

--- client/src/components/Template.jsx
+++ client/src/components/Template.jsx
@@ -11,8 +11,8 @@ export default function Template() {
       <div className='image-container'>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsvDd-BWw08FdNDWctxVXz7FuMdNiZA-wuGA&s" alt="laptop" />
       </div>
-      <div className='add-to-cart'>🛒 add to cart</div>
-      <h5 className='name'>laptop DELL</h5>
+      <div className='add-to-cart'>🛒 Add to cart</div>
+      <h5 className='name'>Laptop DELL</h5>
       <h6 className='price'>960$</h6>
       <h6 className='rating'>⭐⭐⭐⭐⭐(65)</h6>
     </div>
