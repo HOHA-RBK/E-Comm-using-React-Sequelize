3d5131a107d6d6ad82826bbdc260163620e49496
Hosni45 <lahzamimedhosni@gmail.com>
2024-08-01 14:55:15 +0200

    Loading AdminPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,6 @@
-
 import React from 'react';
-
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
-
-import Dashboard from './components/Dashboard.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from './components/Dashboard.jsx';
 import Productlist from './components/Productlist.jsx';
 import Profilepage from './components/Profilepage.jsx';
 import Addproduct from './components/Addproduct.jsx';
@@ -15,30 +8,25 @@ import "./App.css";
 import Signup from "./login & signup/Signup.jsx";
 import Login from "./login & signup/Login.jsx";
 import Test from "./login & signup/Test.jsx";
+import Admindashboard from "./components/adminDash/Admindashboard.jsx";
 
-const App=() =>{
+const App = () => {
   return (
     <div>
       <BrowserRouter>
-                <Routes>
-                    <Route
-                        exact
-                        path="*"
-                        element={<Dashboard />}
-                    />
-<Route
-                        exact
-                        path="*"
-                        element={<Login/>}
-                    />
-                          
-                </Routes>
-            </BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/profile" element={<Profilepage />} />
+          <Route exact path="/products" element={<Productlist />} />
+          <Route exact path="/addproduct" element={<Addproduct />} />
+          <Route exact path="/test" element={<Test />} />
+          <Route exact path="/adminDash" element={<Admindashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+};
+
 export default App;
-
-  
-  
-
-
